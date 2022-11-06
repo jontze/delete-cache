c5bc90224a7769e0ae1e15b4d542ec9ed0c33745
Jontze <42588836+jontze@users.noreply.github.com>
2022-11-06 13:36:42 +0100

    chore(style): Add semicolons and adjust prettier and eslint

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,18 +1,18 @@
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import * as process from 'process';
+import * as cp from 'child_process';
+import * as path from 'path';
+import {expect, test} from '@jest/globals';
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_REPO'] = 'my_repo'
-  process.env['INPUT_KEYS'] = 'cache-key-1'
-  process.env['INPUT_LIMIT'] = '8'
-  process.env['GITHUB_TOKEN'] = '******'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  process.env['INPUT_REPO'] = 'my_repo';
+  process.env['INPUT_KEYS'] = 'cache-key-1';
+  process.env['INPUT_LIMIT'] = '8';
+  process.env['GITHUB_TOKEN'] = '******';
+  const np = process.execPath;
+  const ip = path.join(__dirname, '..', 'lib', 'main.js');
   const options: cp.ExecFileSyncOptions = {
     env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
-})
+  };
+  console.log(cp.execFileSync(np, [ip], options).toString());
+});
